488d8e33d0e673cb8c5095672a8ae748761d5737
gvivan <103859388+gvivan@users.noreply.github.com>
2023-01-03 02:53:27 -0500

    create a new db and collection; insert new entries to the collection

---
+++ app.js
@@ -0,0 +1,40 @@
+const { MongoClient } = require("mongodb");
+// Connection URI
+const uri =
+  "mongodb://localhost:27017";
+// Create a new MongoClient
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    console.log("Connected successfully to server");
+
+    const database = client.db('fruitsDB');
+    const collection = database.collection('fruits');
+
+    await collection.insertMany([
+      {
+        name: "Apple",
+        score: 8,
+        review: "Great Fruit",
+      },
+      {
+        name: "Orange",
+        score: 6,
+        review: "Kinda Sour",
+      },
+      {
+        name: "Banana",
+        score: 9,
+        review: "Great Stuff!",
+      },
+    ])
+
+    console.log("Inserted 3 documents to the collection.");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
