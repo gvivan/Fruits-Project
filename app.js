dcb96735d948c217f822ba3e10697dca48555b19
gvivan <103859388+gvivan@users.noreply.github.com>
2023-01-03 16:21:25 -0500

    refactor code to insert fruits using mongoose

--- app.js
+++ app.js
@@ -1,43 +1,23 @@
-const { MongoClient } = require("mongodb");
-// Connection URI
-const uri =
-  "mongodb://localhost:27017";
-// Create a new MongoClient
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    console.log("Connected successfully to server");
+const mongoose = require('mongoose');
 
-    const database = client.db('fruitsDB');
-    const collection = database.collection('fruits');
+main().catch(err => console.log(err));
 
-    await collection.insertMany([
-      {
-        name: "Apple",
-        score: 8,
-        review: "Great Fruit",
-      },
-      {
-        name: "Orange",
-        score: 6,
-        review: "Kinda Sour",
-      },
-      {
-        name: "Banana",
-        score: 9,
-        review: "Great Stuff!",
-      },
-    ])
-
-    console.log("Inserted 3 documents to the collection.");
-
-    console.log("Found the following records:");
-    console.log(await collection.find().toArray());
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/fruitsDB');
 }
-run().catch(console.dir);
+
+const fruitSchema = new mongoose.Schema ({
+  name: String,
+  rating: Number,
+  reveiw: String
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit ({
+	name: "Apple",
+	rating: 7,
+	review: "Great!"
+});
+
+fruit.save();
